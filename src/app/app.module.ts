13965705a8161808c9d58a00a6dd56e2329e1b84
John A. Fedoruk <johnny@johnfedoruk.ca>
2017-06-21 21:49:49 +0100

    Added ProfileCover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
+import { ProfileCoverComponent } from './partials/profile-cover/profile-cover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProfileCoverComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/partials/profile-cover/profile-cover.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-cover',
+  templateUrl: './profile-cover.component.html',
+  styleUrls: ['./profile-cover.component.css']
+})
+export class ProfileCoverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
