f3cf0419b45210313582ab429021fe20f34868c9
johnfedoruk <johnny@johnfedoruk.ca>
2017-06-22 23:37:00 +0100

    Added jQuery and Sticky Kit. Have a sticky sidebar along Profile's timeline

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+
+declare const $: any;
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+	readonly STICKY:string = "#sidebar";
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	ngAfterViewInit() {
+		// init sticky kit
+		$(this.STICKY).stick_in_parent({
+			recalc_every: 100,
+			offset_top: 15
+		});
+	}
+
+	ngOnDestroy() {
+		// detach sticky kit
+		$(this.STICKY).trigger("sticky_kit:detach");
+	}
 
 }
