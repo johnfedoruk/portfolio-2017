1f9e39f5be5c47862ce6d00e0c6691c40da02d1c
John A. Fedoruk <johnny@johnfedoruk.ca>
2017-06-26 00:15:31 +0100

    Added opening chat box without inner content

--- src/app/partials/chat-box/chat-box.component.ts
+++ src/app/partials/chat-box/chat-box.component.ts
@@ -9,9 +9,19 @@ export class ChatBoxComponent implements OnInit {
 
 	public online:boolean = Math.random()>0.5?true:false;
 
+	public chatBoxOpen:boolean = false;
+
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	public onOpenChatBox() {
+		this.chatBoxOpen = true;
+	}
+
+	public onCloseChatBox() {
+		this.chatBoxOpen = false;
 	}
 
 }
