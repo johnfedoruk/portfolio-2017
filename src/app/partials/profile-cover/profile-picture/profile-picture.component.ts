1ba69194bd3bea3f4bf6c3e3dfaea474a60bfb9e
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 13:53:40 -0600

    Loading profile pic from ProfileService in ProfilePictureComponent

--- src/app/partials/profile-cover/profile-picture/profile-picture.component.ts
+++ src/app/partials/profile-cover/profile-picture/profile-picture.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'app/services/profile.service';
 
 @Component({
-  selector: 'app-profile-picture',
-  templateUrl: './profile-picture.component.html',
-  styleUrls: ['./profile-picture.component.css']
+    selector: 'app-profile-picture',
+    templateUrl: './profile-picture.component.html',
+    styleUrls: ['./profile-picture.component.css']
 })
 export class ProfilePictureComponent implements OnInit {
 
-  constructor() { }
+    public profilePhoto: string;
+    public profilePhotos: string[];
+    constructor(private profile:ProfileService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.profile.getProfilePhotos().subscribe(
+            profilePhotos => {
+                this.profilePhotos = profilePhotos.reverse();
+                this.profilePhoto = this.profilePhotos[0];
+            }
+        )
+    }
 
 }
