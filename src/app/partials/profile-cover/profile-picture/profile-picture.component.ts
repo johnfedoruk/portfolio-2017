ac1994a8fed7382d0b1ef288d22eee7fc56355da
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 14:26:08 -0600

    Handling subscriptions in ProfilePhotoComponent

--- src/app/partials/profile-cover/profile-picture/profile-picture.component.ts
+++ src/app/partials/profile-cover/profile-picture/profile-picture.component.ts
@@ -1,23 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'app/services/profile.service';
+import { Subscription } from 'rxjs/Subscription';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
     selector: 'app-profile-picture',
     templateUrl: './profile-picture.component.html',
     styleUrls: ['./profile-picture.component.css']
 })
-export class ProfilePictureComponent implements OnInit {
+export class ProfilePictureComponent implements OnInit, OnDestroy {
 
     public profilePhoto: string;
     public profilePhotos: string[];
-    constructor(private profile:ProfileService) { }
+    private subscriptions: Subscription[] = [];
+    constructor(private profile: ProfileService) { }
 
     ngOnInit() {
-        this.profile.getProfilePhotos().subscribe(
-            profilePhotos => {
-                this.profilePhotos = profilePhotos.reverse();
-                this.profilePhoto = this.profilePhotos[0];
-            }
+        this.subscriptions.push(
+            this.profile.getProfilePhotos().subscribe(
+                profilePhotos => {
+                    this.profilePhotos = profilePhotos.reverse();
+                    this.profilePhoto = this.profilePhotos[0];
+                }
+            )
+        );
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(
+            subscription => subscription.unsubscribe()
         )
     }
 
