e15d5fb7de6140a92f90cef6cb78d1ac6e434cb1
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 17:38:43 -0600

    Loading emails from ProfileService into OverviewComponent

--- src/app/pages/about/overview/overview.component.ts
+++ src/app/pages/about/overview/overview.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommitService } from 'app/modules/git-graph/services/commit.service';
 import { Subscription } from 'rxjs/Subscription';
-import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { ProfileService } from 'app/services/profile.service';
 
 @Component({
     selector: 'app-overview',
@@ -11,14 +11,20 @@ import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 export class OverviewComponent implements OnInit, OnDestroy {
 
     public commit_count: string;
+    public emails: string[];
     public github: string = "johnfedoruk";
     private subscriptions: Subscription[] = [];
-    constructor(private commits: CommitService) { }
+    constructor(private commits: CommitService, private profile: ProfileService) { }
 
     ngOnInit() {
         this.subscriptions.push(
             this.commits.commitCount(this.github, "http://localhost:9090/").subscribe(
                 commits => this.commit_count = commits
+            )
+        );
+        this.subscriptions.push(
+            this.profile.getEmails().subscribe(
+                emails => this.emails = emails
             )
         );
     }
