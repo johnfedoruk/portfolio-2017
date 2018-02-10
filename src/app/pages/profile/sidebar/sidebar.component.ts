a9efc026e9b2d0b9a774eba0b6b002f0a9444449
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 14:04:26 -0600

    Loading intro from ProfileService in SidebarComponent

--- src/app/pages/profile/sidebar/sidebar.component.ts
+++ src/app/pages/profile/sidebar/sidebar.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'app/services/profile.service';
 
 @Component({
-	selector: 'app-sidebar',
-	templateUrl: './sidebar.component.html',
-	styleUrls: ['./sidebar.component.css']
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
 
-	array:string[] = [];
+    public intro: string;
+    array: string[] = [];
 
-	constructor() { }
+    constructor(private profile: ProfileService) { }
 
-	ngOnInit() {
-		for (let i = 0; i < 15; i++)
-			this.array.push(i % 2 == 0 ? "Hello World Hello World Hello World Hello World" : "World");
-	}
+    ngOnInit() {
+        this.profile.getIntro().subscribe(
+            intro => this.intro = intro
+        )
+        for (let i = 0; i < 15; i++)
+            this.array.push(i % 2 == 0 ? "Hello World Hello World Hello World Hello World" : "World");
+    }
 
 }
