375f15b2ab521b6df3919cb55f2a0873f3eb2899
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 17:35:37 -0600

    Handling subscriptions in OverviewComponent

--- src/app/pages/about/overview/overview.component.ts
+++ src/app/pages/about/overview/overview.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommitService } from 'app/modules/git-graph/services/commit.service';
+import { Subscription } from 'rxjs/Subscription';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
     selector: 'app-overview',
     templateUrl: './overview.component.html',
     styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent implements OnInit, OnDestroy {
 
     public commit_count: string;
     public github: string = "johnfedoruk";
+    private subscriptions: Subscription[] = [];
     constructor(private commits: CommitService) { }
 
     ngOnInit() {
-        this.commits.commitCount(this.github, "http://localhost:9090/").subscribe(
-            commits => this.commit_count = commits
-        )
+        this.subscriptions.push(
+            this.commits.commitCount(this.github, "http://localhost:9090/").subscribe(
+                commits => this.commit_count = commits
+            )
+        );
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(
+            subscription => subscription.unsubscribe()
+        );
     }
 
 }
