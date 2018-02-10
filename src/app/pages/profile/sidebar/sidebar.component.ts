8fe796e46cfec8538fb816e4b23aa2efa6363db6
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 14:23:27 -0600

    Handling subscriptions in SidebarComponent

--- src/app/pages/profile/sidebar/sidebar.component.ts
+++ src/app/pages/profile/sidebar/sidebar.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'app/services/profile.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
 
     public intro: string;
-    array: string[] = [];
+    public array: string[] = [];
+
+    private subscriptions: Subscription[] = [];
 
     constructor(private profile: ProfileService) { }
 
     ngOnInit() {
-        this.profile.getIntro().subscribe(
-            intro => this.intro = intro
-        )
+        this.subscriptions.push(
+            this.profile.getIntro().subscribe(
+                intro => this.intro = intro
+            )
+        );
         for (let i = 0; i < 15; i++)
             this.array.push(i % 2 == 0 ? "Hello World Hello World Hello World Hello World" : "World");
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(
+            subscription => subscription.unsubscribe()
+        )
     }
 
 }
