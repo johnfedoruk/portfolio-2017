2d869424cb1b94ecd62056f81e6efe21feb3fb46
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 14:25:08 -0600

    Handling subscriptions in CoverPhotoComponent

--- src/app/partials/profile-cover/cover-photo/cover-photo.component.ts
+++ src/app/partials/profile-cover/cover-photo/cover-photo.component.ts
@@ -1,29 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'app/services/profile.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-cover-photo',
     templateUrl: './cover-photo.component.html',
     styleUrls: ['./cover-photo.component.css']
 })
-export class CoverPhotoComponent implements OnInit {
+export class CoverPhotoComponent implements OnInit, OnDestroy {
 
     public name: string;
     public coverPhoto: string;
     public coverPhotos: string[];
+    private subscriptions: Subscription[] = [];
 
     constructor(private profile: ProfileService) { }
 
     ngOnInit() {
-        this.profile.getName().subscribe(
-            name => this.name = name
+        this.subscriptions.push(
+            this.profile.getName().subscribe(
+                name => this.name = name
+            )
         );
-        this.profile.getCoverPhotos().subscribe(
-            coverPhotos => {
-                this.coverPhotos = coverPhotos.reverse();
-                this.coverPhoto = this.coverPhotos[0];
-            }
+        this.subscriptions.push(
+            this.profile.getCoverPhotos().subscribe(
+                coverPhotos => {
+                    this.coverPhotos = coverPhotos.reverse();
+                    this.coverPhoto = this.coverPhotos[0];
+                }
+            )
         );
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(
+            subscription => subscription.unsubscribe()
+        )
     }
 
 }
