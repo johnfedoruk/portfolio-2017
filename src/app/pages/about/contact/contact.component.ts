540ab4fc7baec370e29dfdbe29ce558e726ea964
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 14:22:01 -0600

    Handling subscriptions in ContactComponent

--- src/app/pages/about/contact/contact.component.ts
+++ src/app/pages/about/contact/contact.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProfileService } from 'app/services/profile.service';
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
     selector: 'app-contact',
     templateUrl: './contact.component.html',
     styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnInit, OnDestroy {
 
     public phones: string[];
     public emails: string[];
+    private subscriptions: Subscription[] = [];
     constructor(private profile: ProfileService) { }
 
     ngOnInit() {
-        this.profile.getPhones().subscribe(
-            phones=>this.phones = phones
-        )
-        this.profile.getEmails().subscribe(
-            emails=>this.emails = emails
+        this.subscriptions.push(
+            this.profile.getPhones().subscribe(
+                phones => this.phones = phones
+            )
+        );
+        this.subscriptions.push(
+            this.profile.getEmails().subscribe(
+                emails => this.emails = emails
+            )
+        );
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(
+            subscription => subscription.unsubscribe()
         )
     }
 
