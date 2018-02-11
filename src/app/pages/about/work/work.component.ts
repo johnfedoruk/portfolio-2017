4d8fead5d5db02bc99e5118b753850ff800ba10e
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 22:35:58 -0600

    Loading work from ProfileService into WorkComponent

--- src/app/pages/about/work/work.component.ts
+++ src/app/pages/about/work/work.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ProfileService } from 'app/services/profile.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.css']
 })
-export class WorkComponent implements OnInit {
+export class WorkComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    private jobs: any[];
+    private subscriptions: Subscription[] = [];
+    
+    constructor(private profile: ProfileService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.subscriptions.push(
+            this.profile.getWork().subscribe(
+                jobs => this.jobs = jobs.reverse().map(job=>{
+                    job.start = new Date(job.start);
+                    job.start.setDate(job.start.getDate()+1);
+                    if(job.end!==undefined) {
+                        job.end = new Date(job.end);
+                        job.end.setDate(job.end.getDate()+1);
+                    }
+                    return job;
+                })
+            )
+        )
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(
+            subscription => subscription.unsubscribe()
+        )
+    }
 
 }
