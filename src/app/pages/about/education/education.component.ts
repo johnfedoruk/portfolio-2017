35152feb4310de692e742ddc79e22992adcf2be3
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-10 18:06:17 -0600

    Loading education from ProfileService into EducationComponent

--- src/app/pages/about/education/education.component.ts
+++ src/app/pages/about/education/education.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ProfileService } from 'app/services/profile.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
-  selector: 'app-education',
-  templateUrl: './education.component.html',
-  styleUrls: ['./education.component.css']
+    selector: 'app-education',
+    templateUrl: './education.component.html',
+    styleUrls: ['./education.component.css']
 })
-export class EducationComponent implements OnInit {
+export class EducationComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    private educations: any[];
+    private subscriptions: Subscription[] = [];
+    
+    constructor(private profile: ProfileService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.subscriptions.push(
+            this.profile.getEducation().subscribe(
+                educations => this.educations = educations.reverse().map(ed=>{
+                    ed.start = new Date(ed.start);
+                    ed.start.setDate(ed.start.getDate()+1);
+                    if(ed.end!==undefined) {
+                        ed.end = new Date(ed.end);
+                        ed.end.setDate(ed.end.getDate()+1);
+                    }
+                    return ed;
+                })
+            )
+        )
+    }
 
+    ngOnDestroy() {
+        this.subscriptions.forEach(
+            subscription => subscription.unsubscribe()
+        )
+    }
 }
