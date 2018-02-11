0e23e3f9f750b952e2a3ac9d9abd7337ea2ebfe8
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-11 14:28:49 -0600

    Image rendering logic and connection to SlideshowService in SlideshowComponent

--- src/app/partials/slideshow/slideshow.component.ts
+++ src/app/partials/slideshow/slideshow.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SlideshowService } from 'app/services/slideshow.service';
 
 @Component({
     selector: 'app-slideshow',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./slideshow.component.css']
 })
 export class SlideshowComponent implements OnInit {
+    public images: any[];
     public image: string = "http://lifekefunde.com/wp-content/uploads/2018/02/life.jpg";
-    constructor() { }
+    constructor(private slideshow: SlideshowService) { }
 
     ngOnInit() {
+        this.slideshow.images$.subscribe(
+            images =>
+                setTimeout(() => {
+                    this.images = images;
+                    if(this.show())
+                        this.image = this.images[0];
+                }, 1)
+        );
     }
 
+    public closeImages(): void {
+        this.slideshow.close();
+    }
+
+    public show(): boolean {
+        return this.images !== undefined && this.images !== [] && this.images.length !== undefined && this.images.length > 0;
+    }
+
+    public doNothing($event: Event): void {
+        $event.preventDefault();
+        $event.stopPropagation();
+    }
 }
