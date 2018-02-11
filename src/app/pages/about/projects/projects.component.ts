25147771b43f62a9bf5d1ca3f84847b25c5a391e
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-11 00:06:47 -0600

    Loading projects from ProfileService into ProjectsComponent

--- src/app/pages/about/projects/projects.component.ts
+++ src/app/pages/about/projects/projects.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ProfileService } from 'app/services/profile.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    public projects: any[];
+    private subscriptions: Subscription[] = [];
+    constructor(private profile: ProfileService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.subscriptions.push(
+            this.profile.getProjects().subscribe(
+                projects => this.projects = projects.reverse().map(project=>{
+                    project.start = new Date(project.start);
+                    project.start.setDate(project.start.getDate()+1);
+                    if(project.end!==undefined) {
+                        project.end = new Date(project.end);
+                        project.end.setDate(project.end.getDate()+1);
+                    }
+                    return project;
+                })
+            )
+        )
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(
+            subscription => subscription.unsubscribe()
+        );
+    }
 
 }
