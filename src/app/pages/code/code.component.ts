84bfc563d462f2c0d5cb1c6204a6be02947f8c5f
John Fedoruk <johnny@johnfedoruk.ca>
2018-09-02 17:56:25 -0500

    Refactored CodeComponent page and OverviewComponent page to only have NPM and GitHub

--- src/app/pages/code/code.component.ts
+++ src/app/pages/code/code.component.ts
@@ -9,53 +9,13 @@ export class CodeComponent implements OnInit {
 
 	routes: Array<{ title: string; link: string }> = [
 		{
-			title:"Atmega169 Preemptive OS",
-			link:"atmega169-preemptive-os"
+			title: "GitHub",
+			link: "github",
 		},
 		{
-			title:"CTR DRBG",
-			link:"ctr-drbg"
+			title: "NPM",
+			link: "npm",
 		},
-		{
-			title:"AES 128 bit",
-			link:"aes-128-bit"
-		},
-		{
-			title:"Parallel Merge Sort",
-			link:"parallel-merge-sort"
-		},
-		{
-			title: "Merge Sort",
-			link: "merge-sort"
-		},
-		{
-			title: "Quick Sort",
-			link: "quick-sort"
-		},
-		{
-			title: "Radix Sort",
-			link: "radix-sort"
-		},
-		{
-			title: "Heap Sort",
-			link: "heap-sort"
-		},
-		{
-			title: "Binary Search Tree",
-			link: "binary-search-tree"
-		},
-		{
-			title: "Hash Table",
-			link: "hash-table"
-		},
-		{
-			title:"Graphs",
-			link:"graphs"
-		},
-		{
-			title:"Weighted Graphs",
-			link:"weighted-graphs"
-		}
 	];
 
 	constructor() { }
