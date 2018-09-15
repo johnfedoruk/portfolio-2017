67deeb389643fc69c8ecdc24b9d0ff757d750956
John Fedoruk <johnny@johnfedoruk.ca>
2018-09-15 18:49:24 -0500

    TimelineService returning observable that resolves posts

--- src/app/services/timeline.service.spec.ts
+++ src/app/services/timeline.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { TimelineService } from './timeline.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TimelineService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TimelineService]
+      providers: [TimelineService],
+      imports: [
+        HttpClientTestingModule,
+      ]
     });
   });
 
--- src/app/services/timeline.service.ts
+++ src/app/services/timeline.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { filter, first } from 'rxjs/operators';
+import { environment } from 'environments/environment';
 
 @Injectable()
 export class TimelineService {
 
-  constructor() { }
+  private posts$: Subject<any> = new BehaviorSubject<any>(undefined);
+  constructor(private http: HttpClient) {
+    this.load();
+  }
 
+  private async load(): Promise<void> {
+    const url: string = this.url;
+    const tweets = await this.http.get(url).toPromise();
+    this.posts$.next(tweets);
+  }
+
+  public posts(): Observable<any[]> {
+    return this.posts$.pipe(filter((v) => v !== undefined), first());
+  }
+
+  private get url(): string {
+    const url = `${environment.api}/twitter/${environment.username}/tweets`;
+    return url;
+  }
 }
